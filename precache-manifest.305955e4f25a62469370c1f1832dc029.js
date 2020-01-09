131c7ceadaf19204ba7528f63aa63d30fb694c03
DongKeun1 <skj2366@naver.com>
2020-01-10 03:03:44 +0900

    Updates

---
+++ precache-manifest.305955e4f25a62469370c1f1832dc029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d29351e2d050299209e6da68b94ec0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b4a18b7f01ffa8ec2b66",
+    "url": "/movie_app_2020/static/css/main.8157826f.chunk.css"
+  },
+  {
+    "revision": "da26f67d5f691136750c",
+    "url": "/movie_app_2020/static/js/2.75fc28b6.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_2020/static/js/2.75fc28b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4a18b7f01ffa8ec2b66",
+    "url": "/movie_app_2020/static/js/main.7d21bd40.chunk.js"
+  },
+  {
+    "revision": "c1e22dc80131c8cefb55",
+    "url": "/movie_app_2020/static/js/runtime-main.3d665dea.js"
+  }
+]);
